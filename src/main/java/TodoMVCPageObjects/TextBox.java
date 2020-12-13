8fc7089dbc6b16fc9d5ac0a700b3164139cc32bf
Andrey Yershov <andreyyersov424@gmail.com>
2020-12-13 21:38:31 +0200

    delete redundant methods in TextBox.java

--- src/main/java/TodoMVCPageObjects/TextBox.java
+++ src/main/java/TodoMVCPageObjects/TextBox.java
@@ -20,16 +20,4 @@ public class TextBox extends BaseElement
         return getElement(textBoxXpath).getText();
     }
 
-    @Step("TextBox CLASS: verification of the text from element")
-    public void expectedText(String expectedValue)
-    {
-        Assert.assertEquals(getElement(textBoxXpath).getText(), expectedValue);
-    }
-
-    @Step("TextBox CLASS: verify if the element is selected, taking the part of attribute value that contains the text(SELECTED)")
-    public void ifElementSelected(String attributeName, String partOfAttributeName)
-    {
-        Assert.assertTrue(getElement(textBoxXpath).getAttribute(attributeName).contains(partOfAttributeName));
-    }
-
 }
